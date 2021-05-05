a7c6f2d2c270965014d338fb1b131b8bbbc1f1f0
Stefan Trenh <74304707+stefantrenh@users.noreply.github.com>
2021-05-05 21:58:16 +0200

    Update app.js
    much smaller and refactored code

--- Javascript_Slider_v1/app.js
+++ Javascript_Slider_v1/app.js
@@ -9,26 +9,8 @@ let imgArray = [
     , src: "https://live.staticflickr.com/122/316423477_6cbdb14722_h.jpg"}
 ]
 
-let count = -1;
-let sliders = document.querySelectorAll(".slider");
-
-getImg();
-
-function getImg ()
-{
-    for (let i = 0; i < sliders.length; i++) {
-        let imgElement = document.createElement("img");
-        imgElement.setAttribute("src", imgArray[i].src);
-        imgElement.setAttribute("alt", imgArray[i].image);
-        sliders[i].appendChild(imgElement);
-     }
-}
-
-     let imageCookie1 = sliders[0].getElementsByTagName('img')[0].getAttribute("src")
-     let imageCookie2 = sliders[1].getElementsByTagName('img')[0].getAttribute("src")
-     let imageCookie3 = sliders[2].getElementsByTagName('img')[0].getAttribute("src")
-     let imageCookie4 = sliders[3].getElementsByTagName('img')[0].getAttribute("src")
-
+let elemSlide = document.getElementById("slider");
+let counter = -1;
     /*some GDPR confirm... windows.confirm much batter choice than alert, cuz i am lazy and dont want to design*/
     let answer = window.confirm("We are using cookies to store image data, do you wish to enable cookies");
     if (answer) {
@@ -37,44 +19,62 @@ function getImg ()
     else {
         acceptCookies = false;
     }
-
 slider();
 
-function slider() {
-    count++;
+function slider(){
+
     /*Datetime now + set 24 hours, 1 day*/
     let now = new Date();
-    now.setHours(now.getHours() + 24);   
-    if (count == 1){
+
+    now.setHours(now.getHours() + 24);  
+
+    if(counter == 0)
+    {
         if(acceptCookies){
+            let imageCookie1 = elemSlide.getElementsByTagName('img')[0].getAttribute("src", imgArray[0].src)
+            document.cookie = "name = " + imageCookie1 + ";" +   "expires = " + now.toUTCString() +";";
+        }
+        elemSlide.getElementsByTagName('img')[0].setAttribute("src", imgArray[counter].src)
+    }
+    if(counter == 1)
+    {
+        if(acceptCookies){
+            let imageCookie2 = elemSlide.getElementsByTagName('img')[0].getAttribute("src", imgArray[1].src)
+            document.cookie = "name = " + imageCookie2 + ";" +   "expires = " + now.toUTCString() +";";
+        }
+        elemSlide.getElementsByTagName('img')[0].setAttribute("src", imgArray[counter].src)
+    }
+    if(counter == 2)
+    {
+        if(acceptCookies){
+            let imageCookie3 = elemSlide.getElementsByTagName('img')[0].getAttribute("src", imgArray[2].src)
+            document.cookie = "name = " + imageCookie3 + ";" +   "expires = " + now.toUTCString() +";";
+        }
+        elemSlide.getElementsByTagName('img')[0].setAttribute("src", imgArray[counter].src)
+    }
+    if(counter == 3)
+    {
+        if(acceptCookies){            
+        let imageCookie4 = elemSlide.getElementsByTagName('img')[0].getAttribute("src", imgArray[3].src)    
             document.cookie = "name = " + imageCookie4 + ";" +   "expires = " + now.toUTCString() +";";
         }
-        let parent = sliders[3];
-        parent.removeChild(parent.firstChild);
+        elemSlide.getElementsByTagName('img')[0].setAttribute("src", imgArray[counter].src)
+        counter = -1;
     }
-    else if (count == 2){
-        if(acceptCookies){
-        document.cookie = "name = " + imageCookie3 + ";" +  " expires = " + now.toUTCString() +";";
-        }
-        let parent = sliders[2];
-        parent.removeChild(parent.firstChild);
-    }
-    else if  (count == 3){
-        if(acceptCookies){
-        document.cookie = "name = " + imageCookie2 + ";" + " expires = " + now.toUTCString() +";";
-        }
-        let parent = sliders[1];
-        parent.removeChild(parent.firstChild);
-    }
-    else if  (count == 4){
-        if(acceptCookies){
-        document.cookie = "name = " + imageCookie1 + ";" + " expires = " + now.toUTCString() +";";
-        let parent = sliders[0];
-        parent.removeChild(parent.firstChild);
-        getImg();
-        count = 0;
-        }
-    }
-    setTimeout(slider, 3000);
+    counter++;
+    setTimeout(slider,3000)
 }
+
+
+
+
     
+  
+
+
+
+
+
+
+
+
